1e4fb7534962c279a11ce436354146a21c5b9ea8
shajipr21 <shajipr21@gmail.com>
2025-01-09 16:34:56 +0530

    changesz

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -34,9 +34,7 @@
 	</div>
 	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
-	
 @endif
-
 
 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 row-cols-xxl-4">
 
