c43ef2efed6706f7937a9d91c079a793036f03b5
shajipr21 <shajipr21@gmail.com>
2025-01-20 10:45:24 +0530

    otp enabled

--- app/Http/Controllers/User/GeneralSettingsController.php
+++ app/Http/Controllers/User/GeneralSettingsController.php
@@ -105,8 +105,8 @@ public function setScratchOtpEnabled(Request $request)
         // Send the message
 		$bussinessId = 107390568652882;
         $url = 'https://graph.facebook.com/v19.0/'.$bussinessId.'/messages';
-		$token = 'EAAtUQaQveEkBO0gmcNGs7gwa5Q6tch09XviFFSevZAlfUePAuiBHqrY42EdhicnxrQZAPsowjXEARlQaUz2AmoWu7T8rxAxQfWZAE4SjaWvLmazWYd2gscSgC8A1p3dcsJKELfZBW0Kdw9aY3bEYi1PIXSDGjVZA78MCg4Mn0yw76DJYe3rl772KVMgDvKQzp3Sk6svkZB9MhhPkDu';$token=
-		
+		$token = 'EAAtUQaQveEkBO0gmcNGs7gwa5Q6tch09XviFFSevZAlfUePAuiBHqrY42EdhicnxrQZAPsowjXEARlQaUz2AmoWu7T8rxAxQfWZAE4SjaWvLmazWYd2gscSgC8A1p3dcsJKELfZBW0Kdw9aY3bEYi1PIXSDGjVZA78MCg4Mn0yw76DJYe3rl772KVMgDvKQzp3Sk6svkZB9MhhPkDu';
+			
 		$data['mobile_no']="+919995338385";
 		$data['otp']=1234;
 		
