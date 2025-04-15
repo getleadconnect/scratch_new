b34733257d555e85c4219179990a31ef986d31c4
shajipr21 <shajipr21@gmail.com>
2025-04-15 12:47:42 +0530

    fix: capture image

--- resources/views/gl-scratch-web/scratch/index.blade.php
+++ resources/views/gl-scratch-web/scratch/index.blade.php
@@ -461,8 +461,8 @@ input[type="number"]::-ms-reveal {
         $('.kt-login__title').hide()
             var target = document.getElementById('demo');
             html2canvas(target,{
-					  useCORS: true,
 					  allowTaint: true,
+					  foreignObjectRendering: true
 					}).then((canvas) => {
                let a = document.createElement("a");
                 a.download = uniqueId+".png";
